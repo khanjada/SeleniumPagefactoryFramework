d9aac9afccf9a58397758652825ae2560d6291cd
hurayra <khanjada.hurayra.qa@gmail.com>
2019-08-04 00:22:17 -0400

    Contact us page

--- src/main/java/controller/ContactUsPage.java
+++ src/main/java/controller/ContactUsPage.java
@@ -1,4 +1,113 @@
 package controller;
 
-public class ContactUsPage {
+import base.ScriptBase;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+
+public class ContactUsPage extends ScriptBase {
+
+
+    @FindBy (css = "#contact-link > a")
+    WebElement contucts;
+    @FindBy(css = "#center_column > h1")
+    WebElement contactUspageAssert;
+    @FindBy(css = "#columns > div.breadcrumb.clearfix > span.navigation_page")
+    WebElement contact2nd;
+    @FindBy(css = "#center_column > form > fieldset > h3")
+    WebElement sendAmessage;
+    @FindBy(css = "#center_column > form > fieldset > div.clearfix > div.col-xs-12.col-md-3 > p:nth-child(5) > label")
+    WebElement EmailAddress;
+    @FindBy(id = "email")
+    WebElement email;
+    @FindBy(css = "#center_column > form > fieldset > div.clearfix > div.col-xs-12.col-md-3 > div:nth-child(6) > label")
+    WebElement orderRefrenceDisp;
+    @FindBy(id = "id_order")
+    WebElement orderTextBox;
+    @FindBy(css = "#center_column > form > fieldset > div.clearfix > div.col-xs-12.col-md-3 > p:nth-child(7) > label")
+    WebElement AttachFileText;
+    @FindBy(css = "#center_column > form > fieldset > div.clearfix > div.col-xs-12.col-md-3 > p:nth-child(7) > label")
+    WebElement MessageDisp;
+    @FindBy(id = "message")
+    WebElement messageBox;
+    @FindBy(css = "#submitMessage > span")
+    WebElement sendButton;
+    @FindBy(css = "#center_column > div > ol > li")
+    WebElement subjectAssert;
+
+
+    public ContactUsPage(WebDriver driver){
+        PageFactory.initElements(driver,this);
+    }
+
+    public void contuctus() {
+
+        contucts.click();
+        Assert.assertEquals(contactUspageAssert,contactUspageAssert);
+        contact2nd.isDisplayed();
+
+    }
+    public void SendAmessageText() {
+        contucts.click();
+
+        sendAmessage.isDisplayed();
+
+    }
+    public void EmailAddressText() {
+        contucts.click();
+        EmailAddress.isDisplayed();
+
+
+    }
+
+    public void EmailAddressInput(String input) {
+        contucts.click();
+        email.sendKeys(input);
+
+
+    }
+    public void OrderRefrenceAndInput(String put) {
+        contucts.click();
+        orderRefrenceDisp.isDisplayed();
+        orderTextBox.sendKeys(put);
+
+
+    }
+    public void AttachFiletextDisplay() {
+        contucts.click();
+        AttachFileText.isDisplayed();
+
+
+
+    }
+    public void MessageTextdispAndInput(String value) {
+        contucts.click();
+        MessageDisp.isDisplayed();
+        messageBox.sendKeys(value);
+
+
+
+    }
+    public void SendButton() {
+        contucts.click();
+        sendButton.click();
+        Assert.assertEquals(subjectAssert,subjectAssert);
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
--- src/test/java/contactUsPage/ContactUsTest.java
+++ src/test/java/contactUsPage/ContactUsTest.java
@@ -1,4 +1,87 @@
 package contactUsPage;
 
-public class ContactUsTest {
+import base.ScriptBase;
+import controller.ContactUsPage;
+import controller.HomePage;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class ContactUsTest extends ScriptBase {
+
+    ContactUsPage contactUsPage;
+
+
+    @BeforeTest
+    public void beforetest(){
+        init();
+    }
+
+    @Test
+    public void VerifyContuctUs() {
+        contactUsPage = new ContactUsPage (driver);
+        contactUsPage.contuctus();
+
+
+    }
+
+    @Test
+    public void VerifySendAmessagetext() {
+        contactUsPage = new ContactUsPage (driver);
+        contactUsPage.SendAmessageText();
+
+
+    }
+    @Test
+    public void VerifyEmailAdressDisplay() {
+        contactUsPage = new ContactUsPage (driver);
+        contactUsPage.EmailAddressText();
+
+
+    }
+    @Test
+    public void VerifyEmailAdressInput() {
+        contactUsPage = new ContactUsPage (driver);
+        contactUsPage.EmailAddressInput("khanjada@hotmail.com");
+
+
+    }
+    @Test
+    public void VerifyOrderRefrenceAndInput() {
+        contactUsPage = new ContactUsPage (driver);
+        contactUsPage.OrderRefrenceAndInput("janina");
+
+
+    }
+
+
+    @Test
+    public void VerifyAttachFileTextDisplay() {
+        contactUsPage = new ContactUsPage(driver);
+        contactUsPage.AttachFiletextDisplay();
+
+
+    }
+    @Test
+    public void VerifyMessageTextDispAndInput() {
+        contactUsPage = new ContactUsPage(driver);
+        contactUsPage.MessageTextdispAndInput("hi every one");
+
+
+    }
+
+    @Test
+    public void VerifySendButton() {
+        contactUsPage = new ContactUsPage(driver);
+        contactUsPage.SendButton();
+
+
+    }
+
+    @AfterTest
+    public void closebrowser(){
+//        driver.close();
+//        driver.quit();
+    }
+
 }
